648515e2c547544ee6a1f19cc54a435d809bb30e
Eric Kenney <ekenney@spotxchange.com>
2017-08-18 10:19:00 -0600

    More rule changes

--- tslint-config-spotx.js
+++ tslint-config-spotx.js
@@ -5,14 +5,20 @@ module.exports = {
     ],
     "jsRules": {},
     "rules": {
+        "max-line-length": [true, 140],
+        "no-empty": false,
+        "no-reference": false,
         "no-string-literal": false,
         "object-literal-key-quotes": false,
         "object-literal-sort-keys": false,
         "only-arrow-functions": false,
         "one-line": false,
-        "prefer-const"; false,
+        "one-variable-per-declaration": false,
+        "prefer-const": false,
+        "prefer-for-of": false,
         "quotemark": false,
-        "trailing-comma": false
+        "trailing-comma": false,
+        "variable-name": [true, "allow-leading-underscore"]
     },
     "rulesDirectory": []
 };
