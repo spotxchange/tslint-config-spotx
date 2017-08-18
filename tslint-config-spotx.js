140f5d85de11ee675ac5c872f9031af59c957c58
Eric Kenney <ekenney@spotxchange.com>
2017-08-17 22:20:50 -0600

    Some more rule leniency

--- tslint-config-spotx.js
+++ tslint-config-spotx.js
@@ -7,7 +7,10 @@ module.exports = {
     "rules": {
         "no-string-literal": false,
         "object-literal-key-quotes": false,
+        "object-literal-sort-keys": false,
+        "only-arrow-functions": false,
         "one-line": false,
+        "prefer-const"; false,
         "quotemark": false,
         "trailing-comma": false
     },
